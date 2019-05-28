de88ed0c093b4ad8bced475c5c11a005c7908c8d
sangminLee <nalsm98@naver.com>
2019-05-28 17:38:49 +0900

    Data Updata

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Coding Test Tutorial
 이 프로젝트는 코딩 테스트 문제를 풀고 업로드하는 저장소 입니다.
 
+
+
+# 분류
+
+* [올바른 괄호](https://github.com/LeeSM0518/Coding_Test/tree/master/src/correct_parenthesis)
+* [쇠 파이프](https://github.com/LeeSM0518/Coding_Test/tree/master/src/iron_stick)
+* [K 번째 수](https://github.com/LeeSM0518/Coding_Test/tree/master/src/kth_number)
+* [행렬의 곱셈](https://github.com/LeeSM0518/Coding_Test/tree/master/src/matrix_multiplication)
+* [숫자의 표현](https://github.com/LeeSM0518/Coding_Test/tree/master/src/representation_of_numbers)
+* [탑](https://github.com/LeeSM0518/Coding_Test/tree/master/src/tower)
--- src/representation_of_numbers/RepresentNumber.java
+++ src/representation_of_numbers/RepresentNumber.java
@@ -3,15 +3,20 @@ package representation_of_numbers;
 public class RepresentNumber {
 
     static int solution(int n) {
-        int answer = 0;
-        int sum = 0;
-
-        for (int i = 1 ; i < 15; i++) {
-            for (int j = 1 ; j < 15; j++) {
-
+        int answer = 1;
+        int sum;
+        for(int i=0; i<(n/2)+1; i++) {
+            sum = 0;
+            for(int j=i+1; j<=(n/2)+1; j++) {
+                sum += j;
+                if(sum == n) {
+                    answer++;
+                    break;
+                } else if (sum >= n) {
+                    break;
+                }
             }
         }
-
         return answer;
     }
 
