b3c89bf027c64194bf3ba15bf83609ad5b64075c
sangminLee <nalsm98@naver.com>
2019-06-01 21:17:45 +0900

    기능개발 풀기(100%)

--- src/function_development/FunctionDevelopment.java
+++ src/function_development/FunctionDevelopment.java
@@ -1,5 +1,7 @@
 package function_development;
 
+import com.sun.tools.internal.ws.wsdl.document.soap.SOAPUse;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -22,32 +24,40 @@ public class FunctionDevelopment {
         Stack<Project> projects = new Stack<>();
         Queue<Integer> distribute = new LinkedList<>();
 
+        // 스택에 작업 진도를 뒤쪽 인덱스부터 푸시한다.
         for (int i = progresses.length - 1; i >= 0 ; i--) {
             Project project = new Project(progresses[i], speeds[i]);
             projects.push(project);
         }
 
         while (!projects.empty()) {
+            // 프로젝트의 진도를 속도만큼 더 해준다.
             projects.forEach(project -> {
                 project.progress += project.speed;
             });
 
+            // 첫 번째 프로젝트 진도가 100과 같거나 클때
             if (projects.peek().progress >= 100) {
-                int count = (int) projects.stream().filter(project -> project.progress >= 100).count();
-                distribute.add(count);
+                int count = 0;
 
-                for (int i = 0; i < count; i++) {
-                    projects.pop();
+                // 스택에 진도가 100 이상인 것들을 찾아서 pop 해주고 큐에 넣어준다.
+                for (int i = projects.size() - 1; i >= 0; i--) {
+                    if (projects.peek().progress >= 100) {
+                        projects.pop();
+                        count++;
+                    } else {
+                        break;
+                    }
                 }
+                distribute.add(count);
             }
         }
 
         answer = new int[distribute.size()];
 
-        distribute.forEach(distribute -> {});
-
+        // 큐에 들어온 갯수만큼 꺼내온다.
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = (int)distribute.poll();
+            answer[i] = distribute.poll();
         }
 
         return answer;
@@ -57,7 +67,10 @@ public class FunctionDevelopment {
         int[] progresses = new int[]{93, 39, 55};
         int[] speeds = new int[]{1, 30, 5};
 
-        System.out.println(solution(progresses, speeds));
+        int[] answer = solution(progresses, speeds);
+        for(int i : answer) {
+            System.out.print(i + ", ");
+        }
     }
 
 }
