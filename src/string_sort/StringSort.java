9719812a196d47faa8df0791638c7a5a08683277
sangminLee <nalsm98@naver.com>
2019-08-18 18:09:00 +0900

    문자열 내 마음대로 정렬하기

---
+++ src/string_sort/StringSort.java
@@ -0,0 +1,28 @@
+package string_sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class StringSort {
+
+  public String[] solution(String[] strings, int n) {
+    return Arrays.stream(strings)
+        .sorted()
+        .sorted(Comparator.comparing(str -> str.charAt(n)))
+        .toArray(String[]::new);ㄴ
+  }
+
+  public static void main(String[] args) {
+    String[] strings = {"sun", "bed", "car"};
+    int n = 1;
+
+    StringSort stringSort = new StringSort();
+
+    String[] answers = stringSort.solution(strings, n);
+
+    for(String str : answers) {
+      System.out.println(str);
+    }
+  }
+
+}
